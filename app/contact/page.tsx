01eaad8cf4351b6031ecd0377d6ef911da7076bb
jaybamk <jaybamk@gmail.com>
2024-06-28 18:45:24 +0100

    28_06

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     <div className="div">
+      My Next.js App contact page
+     </div>
+     
+    </main>
+  );
+}
